ab542cfb1bae1e96ef478aae78f2764e620d31b5
VikGokli <22vgokli@athenian.org>
2018-10-15 17:24:18 -0700

    1st Commit

---
+++ src/DayOne.java
@@ -0,0 +1,19 @@
+
+impport java.utill.Scanner;
+import java.utill.Array;
+
+
+public class DayOne {
+     public static void main (String[] args){
+          System.out.println("Kill Kyle!!!! Type Yes if you agree");
+          Scanner s = System.in(Next.Int);
+
+
+
+
+
+
+
+
+     }
+}
