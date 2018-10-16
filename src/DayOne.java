d51e2b11b2b408fce5f1b8104788a9aa9340627f
VikGokli <22vgokli@athenian.org>
2018-10-15 17:29:51 -0700

    2nd commit

--- src/DayOne.java
+++ src/DayOne.java
@@ -6,9 +6,10 @@ import java.utill.Array;
 public class DayOne {
      public static void main (String[] args){
           System.out.println("Kill Kyle!!!! Type Yes if you agree");
-          Scanner s = System.in(Next.Int);
+          Scanner s = System.in(next.Int);
 
 
+          //fortnitefriday
 
 
 
